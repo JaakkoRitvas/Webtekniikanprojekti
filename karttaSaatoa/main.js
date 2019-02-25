3e7acbd67f0dc9f583d98c7694501fe9158adc65
shitCodeIsShit <mantymaki.lauri@gmail.com>
2019-02-25 12:39:05 +0200

    Leikkasin kikkelin

--- karttaSaatoa/main.js
+++ karttaSaatoa/main.js
@@ -53,7 +53,7 @@ navigator.geolocation.getCurrentPosition(ifSuccess, error, options);
 
 d3.csv('makkarit.csv').then(function(data){
     for(i = 0; i < data.length; i++){
-        L.marker([kikkeli])
+        L.marker([])
         console.log(data[i]);
         console.log('Voi vittujen keväät');
 
